913044f9283cdf2483fb4d895006370a2b983850
Nitin <nsinghboss02@gmail.com>
2022-02-27 17:30:24 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,25 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(palindrome(676));
+
+    }
+    public static boolean palindrome(int n){
+        Stack<Integer> stack = new Stack<>();
+        int rem=0,rev=0,temp = n;
+        while(temp!=0){
+            rem = temp%10;
+            rev = rev*10 + rem;
+            temp = temp/10;
+        }
+        stack.push(rev);
+        if(stack.pop()==n){
+            return true;
+        }
+        return false;
+    }
+
+}
