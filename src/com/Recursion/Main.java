631e0e07e3f26cfda30b3c16db80c0926e4f23a7
Nitin <nsinghboss02@gmail.com>
2022-02-27 18:59:00 +0530

    qs added

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// checking the number is palindrome or not by using stack.
         System.out.println(palindrome(676));
-
     }
     public static boolean palindrome(int n){
         Stack<Integer> stack = new Stack<>();
@@ -21,5 +20,4 @@ public class Main {
         }
         return false;
     }
-
 }
